100c71ef73bc1d7ab194003e688428db2841a388
vladk <vlad_krasovsky@yahoo.com>
2021-01-31 10:53:18 -0800

    initial commit

---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type JumbotronPropsType = {};
+
+const Jumbotron: React.FC<JumbotronPropsType> = (props) => {
+  return <div>Jumbotron Component</div>;
+};
+export default Jumbotron;
