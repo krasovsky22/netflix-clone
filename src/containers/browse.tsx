07b6f5d182b98a48bf69c47e3fd3d2512a098179
vladk <vlad_krasovsky@yahoo.com>
2021-02-07 21:11:56 -0800

    added fuse search

--- src/containers/browse.tsx
+++ src/containers/browse.tsx
@@ -1,4 +1,5 @@
 import { Card, Header, Loading, Player } from '@/components';
+import Fuse from 'fuse.js';
 import { BROWSE } from '@/constants/routes';
 import { SelectProfilesContainer } from '@/containers';
 import { useAuthListener } from '@/hooks';
@@ -42,6 +43,20 @@ const BrowseContainer: React.FC<BrowseContainerPropsType> = ({ slides }) => {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    });
+
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   if (profile.displayName !== null) {
     return (
